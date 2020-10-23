b10694f35bbc210e6667c926d99bc78d5b35ac01
Minh Quang Pham <qquang24t5@gmail.com>
2020-10-23 12:17:04 +0700

    Thêm Spring boot

---
+++ src/main/java/sgu/nangcafe/NangCafeApplication.java
@@ -0,0 +1,13 @@
+package sgu.nangcafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NangCafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NangCafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sgu/nangcafe/NangCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package sgu.nangcafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NangCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
